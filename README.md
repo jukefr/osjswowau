84740fad2e6825514ee861231212d50bf6888976
Kevin Jullien <kevin.j@lajavaness.com>
2020-12-10 14:35:10 +0100

    mvp

--- README.md
+++ README.md
@@ -1,20 +1,81 @@
 # osjswowau
 open source javascript wow addon updater
 
-## supported
-- curse
-- elvui + elvui addons
+it's also cross-platform (windows/linux/macos)
 
-## usage (currently not published, see #dev)
+![demo gif](https://i.imgur.com/aBorq7v.gif)
+
+## requirements
+- node.js lts or newer
+
+## supported
+- curse **(latest, md5 checked)**
+- elvui + elvui addons **(latest, no hashes available)**
+
+## try it out
+**you will need to run it twice (it downloads 130Mb of chromium on every execution, so slow connections may favor installation and usage section)**
+
+the first time we bootstrap the configuration file and kill the process to give you some time to edit it
 ```bash
 $ npx osjswowau
 ```
 
-## dev
+## installation and usage
 ```bash
-$ git clone ... && cd ...
-$ npx .
+$ npm i -g osjswowau # might need --unsafe-perm on linux
+
+$ osjswowaus
 ```
 
-## debug
-you can toggle `debug` to `true` in the config to get more information if something goes wrong and for bug reports.
+## how to configure
+if you have a good connection the default settings (except `realpath` and `addons`) will probably work fine for you
+
+otherwise, try simply re-running the command a second time before changing the configuration, web pages can be finicky...
+
+the first time you use the app it will tell you where the configuration file is located
+```js
+{
+    realpath: "/home/user/Games/world-of-warcraft/drive_c/Program Files (x86)/World of Warcraft/_retail_/Interface/AddOns",
+    timeout: 30000, // will depend on your connection and state of sites
+    polling: 1000, // recommended
+    waitAfterNavig: 2000, // will depend on your connection and state of sites
+    tmp: "./tmp",
+    debug: false,
+    addons: {
+        curse: [
+            "azeroth-auto-pilot",
+            "big-wigs",
+            "details",
+            "little-wigs",
+            "pawn",
+            "plater-nameplates",
+            "tradeskill-master",
+            "weakauras-2",
+        ],
+        elvui: [ // having this section will enable elvui updates (or not)
+            137, // floating combat text
+            38, // shadow and light
+            3, // addon skins
+        ],
+    },
+}
+```
+
+### curse addons
+simply use the name of the extension from the curse site
+```
+https://www.curseforge.com/wow/addons/plater-nameplates/...
+-> plater-nameplates
+```
+
+### elvui and elvui addons
+having an `elvui` array in your `addons` config section will automatically enable updating elvui
+
+you need the addon id as they do not provide names ( or hashes :( )
+```
+https://www.tukui.org/addons.php?id=137
+-> 137
+```
+
+## future
+once more error cases are handled i can start to parallelize (currently implemented on *nix)
--- index.js
+++ index.js
@@ -1,228 +1,64 @@
 Promise = require("bluebird");
-const { mkdir, rmdir } = require("fs").promises;
-const { existsSync, createReadStream, statSync } = require("fs");
 const puppeteer = require("puppeteer-extra");
-const moment = require("moment");
-const md5File = require("md5-file");
-const unzipper = require("unzipper");
-const glob = require("glob-promise");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-const chalk = require("chalk");
 const Conf = require("conf");
-const { join, dirname } = require("path");
+const {join, dirname} = require("path");
+const cliProgress = require('cli-progress');
+const {firstStart, cleanTmps} = require('./utils')
+const {elvuiLogic} = require('./elvui')
+const {curseLogic} = require('./curse')
 puppeteer.use(StealthPlugin());
 
-const delay = (d) =>
-  new Promise(function (resolve) {
-    setTimeout(resolve, d);
-  });
-
-const log = {
-  error: (...msg) => console.log(chalk.red(...msg)),
-  info: (...msg) => console.log(chalk.green(...msg)),
-  debug: (...msg) => console.log(chalk.blue(...msg)),
-};
-
 const main = async () => {
-  const config = new Conf({
-    defaults: {
-      realpath:
-        "/home/user/Games/world-of-warcraft/drive_c/Program Files (x86)/World of Warcraft/_retail_/Interface/AddOns",
-      timeout: 30000, // will depend on your connection and state of sites
-      polling: 1000, // recommended
-      waitAfterNavig: 2000, // will depend on your connection and state of sites
-      tmp: "./tmp",
-      debug: false,
-      addons: {
-        curse: [
-          "azeroth-auto-pilot",
-          "big-wigs",
-          "details",
-          "little-wigs",
-          "pawn",
-          "plater-nameplates",
-          "tradeskill-master",
-          "weakauras-2",
-        ],
-        elvui: [ // having this section will enable elvui updates (or not)
-          137, // floating combat text
-          38, // shadow and light
-          3, // addon skins
-        ],
-      },
-    },
-  });
+    try {
 
-  if (
-    config.get("realpath") ===
-      "/home/user/Games/world-of-warcraft/drive_c/Program Files (x86)/World of Warcraft/_retail_/Interface/AddOns" &&
-    config.get("tmp") !== "./foobar"
-  ) {
-    log.info("We have detected that this is your first usage.");
-    log.info(`Please edit ${chalk.yellow(config.path)} to match your needs.`);
-    process.exit(1);
-  }
-  try {
-    const cfg = {
-      ...config.store,
-      tmp: join(dirname(config.path), config.get("tmp")),
-    };
+        const multibar = new cliProgress.MultiBar({
+            clearOnComplete: false,
+            format: '[{bar}] {percentage}% | {value}/{total} | {filename}',
+            hideCursor: true
+        }, cliProgress.Presets.shades_grey);
 
-    if (!existsSync(cfg.tmp)) await mkdir(cfg.tmp);
+        const config = new Conf({});
 
-    return puppeteer.launch({ headless: !cfg.debug }).then(async (browser) => {
-      const curseLogic = async (b, name) => {
-        const wait = async (f, m) => {
-          const start = moment();
-          while (moment().diff(start, "ms") < cfg.timeout) {
-            if (existsSync(f)) {
-              const md5 = await md5File(f);
-              if (md5 === m) {
-                return true;
-              }
-            }
-            await delay(cfg.polling);
-          }
+        firstStart(config)
+
+        const cfg = {
+            ...config.store,
+            tmp: join(dirname(config.path), config.get("tmp")),
         };
 
-        log.info("downloading", name);
+        return puppeteer.launch({headless: !cfg.debug}).then(async (browser) => {
+            await Promise.map(
+                cfg.addons.curse,
+                (addon) => curseLogic(browser, addon, multibar, cfg),
+                {
+                    concurrency: process.platform === 'win32' ? 1 : 2, // WARN: windows buggy...
+                }
+            );
 
-        const page = await b.newPage();
-        await page._client.send("Page.setDownloadBehavior", {
-          behavior: "allow",
-          downloadPath: cfg.tmp,
-        });
-        await page.goto(`https://www.curseforge.com/wow/addons/${name}/files`);
-
-        await delay(cfg.waitAfterNavig);
-        const filepath = await page.$eval(
-          "div.flex-row:nth-child(1) > span:nth-child(2)",
-          (x) => x.innerText
-        );
-        const md5 = await page.$eval(
-          "div.flex:nth-child(7) > span:nth-child(2)",
-          (x) => x.innerText
-        );
-        await Promise.all([
-          page.$eval(
-            "article.box > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1)",
-            (x) => x.click()
-          ),
-          wait(`${cfg.tmp}/${filepath}`, md5),
-        ]).catch((err) => log.error(name, err));
-
-        log.info("extracting", name);
-
-        await new Promise((resolve, reject) =>
-          createReadStream(`${cfg.tmp}/${filepath}`)
-            .pipe(unzipper.Extract({ path: cfg.realpath }))
-            .on("close", (err) => (err ? reject(err) : resolve()))
-        );
-
-        log.info("finished", name);
-        await page.close();
-        return true;
-      };
-
-      const elvuiLogic = async (b, name = "elvui") => {
-        const wait = async (f, m) => {
-          const start = moment();
-          let size;
-
-          while (moment().diff(start, "ms") < cfg.timeout) {
-            if (m === "elvui") {
-              [f] = await glob(`${cfg.tmp}\-elvui/elvui-*.zip`);
-              cfg.debug && log.debug(f);
-            } else {
-              [f] = await glob(`${cfg.tmp}\-${name}/*.zip`);
-              cfg.debug && log.debug(f);
+            if (cfg.addons.elvui) {
+                await Promise.map(
+                    ["elvui", ...cfg.addons.elvui],
+                    (addon) => elvuiLogic(browser, addon, multibar, cfg),
+                    {
+                        concurrency: 1, // TODO: chrome "page" set download really is on instance level
+                    }
+                )
             }
-            if (existsSync(f)) {
-              if (size && size !== 0 && size === statSync(f).size) return f;
-              size = statSync(f).size;
-              cfg.debug && log.debug(name, "size", size);
-            }
-            cfg.debug && log.debug(name, "waiting", cfg.polling, "ms");
-            await delay(cfg.polling);
-          }
-
-          throw new Error("this shouldnt happen");
-        };
-
-        log.info("downloading", name);
-
-        const page = await b.newPage();
-        await page._client.send("Page.setDownloadBehavior", {
-          behavior: "allow",
-          downloadPath: `${cfg.tmp}\-${name}`,
+            return Promise.all([
+                browser.close(),
+                multibar.stop()
+            ]).then(() => cleanTmps(cfg));
         });
 
-        if (name === "elvui") {
-          await page.goto("https://www.tukui.org/download.php?ui=elvui");
-        }
+    } catch (err) {
+        console.log(chalk.red('Something went terribly wrong. Usually timeouts and a simple re-run of the command fixes it.'))
+        console.log(chalk.red('Enable debug mode to learn more.'))
+        process.exit(1)
+    }
 
-        if (Number.isInteger(name)) {
-          cfg.debug && log.debug(`https://www.tukui.org/addons.php?id=${name}`);
-          await page.goto(`https://www.tukui.org/addons.php?id=${name}`);
-        }
-
-        await delay(cfg.waitAfterNavig);
-
-        let hm;
-        if (name === "elvui") {
-          [_, hm] = await Promise.all([
-            page.$eval("#download > div > div > a", (x) => x.click()),
-            wait("", name),
-          ]);
-        } else {
-          [_, hm] = await Promise.all([
-            page.$eval("div.col-md-3:nth-child(3) > a:nth-child(1)", (x) =>
-              x.click()
-            ),
-            wait("", name),
-          ]).catch((err) => log.error(name, err));
-        }
-        log.info("extracting", name, hm);
-
-        await new Promise((resolve, reject) =>
-          createReadStream(hm)
-            .pipe(unzipper.Extract({ path: cfg.realpath }))
-            .on("close", (err) => (err ? reject(err) : resolve()))
-        );
-
-        log.info("finished", name);
-        return page.close();
-      };
-
-      await Promise.map(
-        cfg.addons.curse,
-        (addon) => curseLogic(browser, addon),
-        {
-          concurrency: 2,
-        }
-      );
-
-      if (cfg.addons.elvui) {
-        await Promise.map(
-          ["elvui", ...cfg.addons.elvui],
-          (addon) => elvuiLogic(browser, addon),
-          {
-            concurrency: 1, // WARN: chrome "page" set download really is on instance level
-          }
-        );
-      }
-
-      await browser.close();
-      const tmps = await glob(`${cfg.tmp}\*`);
-      return Promise.map(tmps, async (d) => rmdir(d, { recursive: true }));
-    });
-  } catch (err) {
-    !cfg.debug && log.err("There was an error. Try enabling debug mode.");
-    cfg.debug && log.debug(tmps);
-    cfg.debug && log.err(err);
-    cfg.debug && log.debug(err.trace);
-    process.exit(1);
-  }
 };
+
+module.exports = main
 
 main();
